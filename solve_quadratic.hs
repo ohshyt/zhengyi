398f6d6286588e45def7cb936b62992851b89405
ohshyt <ueatshyt@gmail.com>
2017-02-27 16:40:13 +1100

    Update solve_quadratic.hs

--- solve_quadratic.hs
+++ solve_quadratic.hs
@@ -1,5 +1,7 @@
 module Solve_Quadratic
     where
+    x1 = (-b-sqrt(b^2-4*a*c))/(2*a)
+    x2 = (-b+sqrt(b^2-4*a*c))/(2*a)
 
 solve_quadratic :: Float -> Float -> Float -> (Float -> Float)
-solve_quadratic a b c = (0.0, 0.0)
+solve_quadratic a b c = (x1, x2)
